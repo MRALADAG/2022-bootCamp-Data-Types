db24ef5cc28417f2140b226fe39867c104bcba6f
MRALADAG <mstf_aladag@yahoo.com>
2022-09-17 10:36:01 +0300

    Variables exercises

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        char karak = '£';
+        char a = '$';
+        int deger = karak;
+        int deger2 = (int) a;
+        int TL = 0x20BA;
+        System.out.println("deger => " + deger);
+        System.out.println("deger2 => " + deger2);
+        System.out.println("TL => " + (char) TL);
+    }
+}
